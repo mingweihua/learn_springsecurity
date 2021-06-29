a098cdcbd35d4ecf5e83766a0a191cf573b68335
ming <984396692@qq.com>
2021-06-29 20:56:14 +0800

    init

---
+++ src/main/java/cn/sysu/controller/TestController.java
@@ -0,0 +1,14 @@
+package cn.sysu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello,SpringSecurity";
+    }
+}
