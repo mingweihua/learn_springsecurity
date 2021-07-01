542244b9fcf9d07cdef72c97bd5490d83e4838e7
ming <984396692@qq.com>
2021-07-01 23:00:20 +0800

    0701-ming

--- src/main/java/cn/sysu/config/SecurityConfig2.java
+++ src/main/java/cn/sysu/config/SecurityConfig2.java
@@ -32,6 +32,18 @@ public class SecurityConfig2 extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        //退出登录
+        http.logout()
+                //退出登录路径
+                .logoutUrl("/logout")
+                //退出登录成功后跳转路径
+                .logoutSuccessUrl("/test/log_success")
+                //允许任意权限的人操作
+                .permitAll();
+
+        //自定义无权限跳转的页面，即403
+        http.exceptionHandling().accessDeniedPage("/unauth.html");
+
         //自定义登录页面
         http.formLogin()
                 //登录页面设置
@@ -39,6 +51,16 @@ public class SecurityConfig2 extends WebSecurityConfigurerAdapter {
                 //登录访问路径
                 .loginProcessingUrl("/login")
                 //登录成功后跳转路径
-                .defaultSuccessUrl("/index").permitAll();
+                .defaultSuccessUrl("/index").permitAll()
+                .and().authorizeRequests()
+                //设置哪些路径可以通过
+                .antMatchers("/login","/test/hello").permitAll()
+                //设置拥有哪些权限的人可以访问
+                .antMatchers("/admin/index").hasAuthority("admin")
+                //设置拥有哪些权限的人可以访问(多种权限类型)
+                .antMatchers("/admin/index","admin/index2").hasAnyAuthority("admin,superuser")
+                .anyRequest().authenticated()
+                //关闭csrf防护
+                .and().csrf().disable();
     }
 }
